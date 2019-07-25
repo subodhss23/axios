27d891d754e1a89f0e0f8a4fa6df4713ea9fa2b8
subodhss23@gmail.com <subodhss23@gmail.com>
2019-07-24 17:07:18 -0700

    get and post request created

--- main.js
+++ main.js
@@ -5,8 +5,8 @@ function performGetRequest1(){
   resultElement.innerHTML = " ";
 
 axios.get('http://jsonplaceholder.typicode.com/todos')
-  .then((response) => resultElement.innerHTLM = generateSucessHTMLOutput(response))
-  .catch((error) => resultElement.innterHTLM = generateErrorHTMLOutput(error));
+  .then((response) => resultElement.innerHTML = generateSucessHTMLOutput(response))
+  .catch((error) => resultElement.innterHTML = generateErrorHTMLOutput(error));
 }
 
 function generateSucessHTMLOutput(response){
@@ -29,4 +29,42 @@ function generateErrorHTMLOutput(error){
           '<pre>' + JSON.stringify(error.response.headers, null, '\t') + '</pre>' + 
           '<h5>Data:</h5>' + 
           '<pre>' + JSON.stringify(error.response.data, null, '\t') + '</pre>'; 
+}
+
+function performGetRequest2(){
+  var resultElement = document.getElementById('getResult2');
+  var todoId = document.getElementById('todoId').nodeValue;
+  resultElement.innerHTML = " ";
+
+  axios.get('http://jsonplaceholder.typicode.com/todos', {
+    params: {
+      id: todoId
+    }
+  })
+  .then((response) => {
+    console.log(response);
+    resultElement.innterHTML = generateSucessHTMLOutput(response);
+  })
+  .catch((error) => resultElement.innterHTML = generateErrorHTMLOutput(error));
+}
+
+document.getElementById('todoInputForm').addEventListener('submit', performPostRequest)
+
+function performPostRequest(e){
+  var resultElement = doucment.getElementById('postResult');
+  var todoTitile = doucment.getElementById('todoTitle').value;
+  resultElement.innerHTML=' ';
+
+  axios.post('http://jsonplaceholder.typicode.com/todos', {
+    userId: '1',
+    title: todoTitile,
+    completed: false
+  })
+  .then((response) => {
+    resultElement.innterHTML = denterateSucressHTMLOutput(response);
+  })
+  .catch((error) => resultElement.innterHTML = generateErrorHTMLOutput(error)
+    
+  );
+  e.preventDefault();
 }
